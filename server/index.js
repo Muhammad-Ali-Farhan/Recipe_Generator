93d71512807007d970b43d9790e09d647b14170e
Muhammad-Ali-Farhan <moh.alifarhan@gmail.com>
2024-10-14 01:07:26 -0500

    Update

--- server/index.js
+++ server/index.js
@@ -1,14 +1,15 @@
-const express = require("express");
-const cors = require("cors");
-const { getRecipes } = require("./controllers/recipeController");
-require("dotenv").config();
-
+const express = require('express');
+const path = require('path');
 const app = express();
-app.use(cors());
-
-app.get("/api/recipes", getRecipes);
-
 const PORT = process.env.PORT || 5000;
+
+
+app.use(express.static(path.join(__dirname, '../client/client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/client/build', 'index.html'));
+});
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
