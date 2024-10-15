18ae6da837e29b50835eb5d6e0d88047b13d79e8
Muhammad Ali Farhan <moh.alifarhan@gmail.com>
2024-10-15 11:01:41 -0500

    Update

--- server/index.js
+++ server/index.js
@@ -5,15 +5,17 @@ const { getRecipes } = require("./controllers/recipeController");
 require("dotenv").config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: process.env.NODE_ENV === 'production' ? 'https://recipe-generator-12.onrender.com/' : 'http://localhost:3000' 
+}));
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, '../client/client/build'))); // Adjusted path
+
+app.use(express.static(path.join(__dirname, '../client/client/build'))); 
 
 app.get("/api/recipes", getRecipes);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../client/client/build/index.html')); // Adjusted path
+    res.sendFile(path.join(__dirname, '../client/client/build/index.html')); 
 });
 
 const PORT = process.env.PORT || 5000;
